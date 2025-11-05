32811004e67b132607bcc5110e4be387f6fd9fb4
Leo <leovieux.pro@gmail.com>
2025-11-05 13:26:28 +0100

    mdp admin manuel

--- databaseScript.sql
+++ databaseScript.sql
@@ -55,4 +55,4 @@ ALTER TABLE utilisateurs ADD COLUMN role ENUM('admin', 'utilisateur') DEFAULT 'u
 -- Insertion du premier admin dans la base de donnée
 
 INSERT INTO utilisateurs (login, mot_de_passe, role)
-VALUES ('admin', 'SuperMotDepasseQuiEstCoolToyota', 'admin');
+VALUES ('admin', '$2y$10$8mwix6D..h4fpWWX.rv6.c7ZO6zBfzlo1pmqsxv6JstEDgVGHAK.', 'admin');
