d29c9c320a6f8cbf61c9bd018498111f4c49ee2e
Leo <leovieux.pro@gmail.com>
2025-11-05 12:43:54 +0100

    Base de index et de ListeClients ddans vue.

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Bienvenue dans la gestion des commandes</h1>
+    <div>
+        <a href="./vues/ListeClients.php"><button>Gérer les Clients</button></a>
+        <a href=""><button>Gérer les Produits</button></a>
+        <a href=""><button>Gérer les Commandes</button></a>
+    </div>
+</body>
+</html>
