c6a891a0b8f9c4e84cf9fca12dc8a2e226f69961
Leo <leovieux.pro@gmail.com>
2025-11-05 13:25:38 +0100

    database ajout

---
+++ database.php
@@ -0,0 +1,21 @@
+<?php
+// Fichier de connexion à la base de données
+class Database {
+    private $host = "127.0.0.1";
+    private $db_name = "gestion_commandes";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $exception) {
+            echo "Erreur de connexion : " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
+?>
